b6446509b41607a723858017866169df246f5fc4
Guillermo Guerrero <g.guerrero.bus@gmail.com>
2012-07-08 02:27:33 +0200

    Done basic I18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,35 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :set_locale
+   
+  def set_locale
+    I18n.locale = extract_locale_from_tld || I18n.default_locale
+  end
+   
+  # Get locale from top-level domain or return nil if such locale is not available
+  # You have to put something like:
+  #   127.0.0.1 application.org
+  #   127.0.0.1 es.application.org
+  #   127.0.0.1 de.application.org
+  # in your /etc/hosts file to try this out locally
+  def extract_locale_from_tld
+    parsed_locale = request.domain.split('.').first
+    I18n.available_locales.include?(parsed_locale.to_sym) ? parsed_locale : nil
+  end
+  
+  def default_url_options(options = {})
+    logger.debug "default_url_options is passed options: #{options.inspect}\n"
+    { :locale => I18n.locale }
+  end
+  
+  # def set_locale
+  #   logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
+  #   I18n.locale = extract_locale_from_accept_language_header
+  #   logger.debug "* Locale set to '#{I18n.locale}'"
+  # end
+
+  # private
+  # def extract_locale_from_accept_language_header
+  #   request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+  # end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -46,16 +46,22 @@ Greenicecube::Application.routes.draw do
   #     resources :products
   #   end
 
+  scope ":locale" do
+    resources :users
+  end
+
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-  root to: 'pages#home'
+  match ":locale", to: 'pages#home'
+  root             to: 'pages#home'
 
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+  match ":locale/home", to: 'pages#home'
   match "/home",    to: 'pages#home'
   match "/about",   to: 'pages#about'
   match "/contact", to: 'pages#contact'
